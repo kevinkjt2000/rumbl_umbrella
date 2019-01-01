4677183d4bf8efa270f431b472da945eb14e3cf7
Kevin Tindall <kevinkjt2000@gmail.com>
2019-01-01 10:26:09 -0600

    Display validation errors when creating users

--- lib/rumbl/accounts/accounts.ex
+++ lib/rumbl/accounts/accounts.ex
@@ -6,6 +6,12 @@ defmodule Rumbl.Accounts do
   alias Rumbl.Accounts.User
   alias Rumbl.Repo
 
+  def create_user(attrs \\ %{}) do
+    %User{}
+    |> User.changeset(attrs)
+    |> Repo.insert()
+  end
+
   def change_user(%User{} = user) do
     User.changeset(user, %{})
   end
