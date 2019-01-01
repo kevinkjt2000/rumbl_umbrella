0bc993ec2fea12d02f1e42727ceb27b07c0dd4f0
Kevin Tindall <kevinkjt2000@gmail.com>
2019-01-01 11:28:44 -0600

    Automatically login new accounts

--- lib/rumbl_web/controllers/auth.ex
+++ lib/rumbl_web/controllers/auth.ex
@@ -3,6 +3,13 @@ defmodule RumblWeb.Auth do
 
   alias Rumbl.Accounts
 
+  def login(conn, user) do
+    conn
+    |> assign(:current_user, user)
+    |> put_session(:user_id, user.id)
+    |> configure_session(renew: true)
+  end
+
   def init(opts), do: opts
 
   def call(conn, _opts) do
