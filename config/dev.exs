51834e8783cd56b963d4e447dd51525b64aa7189
Kevin Tindall <kevinkjt2000@gmail.com>
2019-01-13 21:17:49 -0600

    Change ports

--- config/dev.exs
+++ config/dev.exs
@@ -7,7 +7,7 @@ use Mix.Config
 # watchers to your application. For example, we use it
 # with webpack to recompile .js and .css sources.
 config :rumbl, RumblWeb.Endpoint,
-  http: [port: 4000],
+  http: [port: 4001],
   debug_errors: true,
   code_reloader: true,
   check_origin: false,
@@ -72,4 +72,5 @@ config :rumbl, Rumbl.Repo,
   password: "postgres",
   database: "rumbl_dev",
   hostname: "localhost",
+  port: 5433,
   pool_size: 10
