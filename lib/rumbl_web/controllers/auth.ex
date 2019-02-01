a88a2db66a25d4999ec1f4898495d0fdeb20bdde
Kevin Tindall <kevinkjt2000@gmail.com>
2019-01-31 19:50:20 -0600

    Test listing of user's videos

--- lib/rumbl_web/controllers/auth.ex
+++ lib/rumbl_web/controllers/auth.ex
@@ -5,6 +5,21 @@ defmodule RumblWeb.Auth do
   alias Rumbl.Accounts
   alias RumblWeb.Router.Helpers, as: Routes
 
+  def call(conn, _opts) do
+    user_id = get_session(conn, :user_id)
+
+    cond do
+      user = conn.assigns[:current_user] ->
+        conn
+
+      user = user_id && Accounts.get_user(user_id) ->
+        assign(conn, :current_user, user)
+
+      true ->
+        assign(conn, :current_user, nil)
+    end
+  end
+
   def authenticate_user(conn, _opts) do
     if conn.assigns.current_user do
       conn
