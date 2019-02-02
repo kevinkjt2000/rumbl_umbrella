36179b0554f281018e43108da19a37b0f1d01a91
Kevin Tindall <kevinkjt2000@gmail.com>
2019-02-01 22:05:18 -0600

    Add negative auth tests

--- test/rumbl_web/controllers/auth_test.exs
+++ test/rumbl_web/controllers/auth_test.exs
@@ -11,6 +11,22 @@ defmodule RumblWeb.AuthTest do
     {:ok, %{conn: conn}}
   end
 
+  test "login with a valid username and pass", %{conn: conn} do
+    user = user_fixture(username: "me", email: "me@test", password: "secret")
+    {:ok, conn} = Auth.login_by_email_and_pass(conn, "me@test", "secret")
+
+    assert conn.assigns.current_user.id == user.id
+  end
+
+  test "login with a not found user", %{conn: conn} do
+    assert {:error, :not_found, _conn} = Auth.login_by_email_and_pass(conn, "me@test", "secret")
+  end
+
+  test "login with password mismatch", %{conn: conn} do
+    _ = user_fixture(username: "me", email: "me@test", password: "secret")
+    assert {:error, :unauthorized, _conn} = Auth.login_by_email_and_pass(conn, "me@test", "wrong")
+  end
+
   test "call with no session sets current_user assign to nil", %{conn: conn} do
     conn = Auth.call(conn, Auth.init([]))
     assert conn.assigns.current_user == nil
