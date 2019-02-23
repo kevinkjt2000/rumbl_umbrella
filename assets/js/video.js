86fce6b4aa59f0455a92cc59a202e77a37bea229
Kevin Tindall <kevinkjt2000@gmail.com>
2019-02-22 20:25:25 -0600

    Handle disconnects gracefully by syncing to the last_seen_id

--- assets/js/video.js
+++ assets/js/video.js
@@ -17,7 +17,10 @@ let Video = {
     let msgContainer = document.getElementById("msg-container");
     let msgInput = document.getElementById("msg-input");
     let postButton = document.getElementById("msg-submit");
-    let vidChannel = socket.channel("videos:" + videoId);
+    let lastSeenId = 0;
+    let vidChannel = socket.channel("videos:" + videoId, () => {
+      return { last_seen_id: lastSeenId };
+    });
 
     postButton.addEventListener("click", e => {
       let payload = { body: msgInput.value, at: Player.getCurrentTime() };
@@ -28,6 +31,7 @@ let Video = {
     });
 
     vidChannel.on("new_annotation", resp => {
+      lastSeenId = resp.id;
       this.renderAnnotation(msgContainer, resp);
     });
 
@@ -44,9 +48,13 @@ let Video = {
 
     vidChannel
       .join()
-      .receive("ok", resp =>
-        this.scheduleMessages(msgContainer, resp.annotations)
-      )
+      .receive("ok", resp => {
+        let ids = resp.annotations.map(ann => ann.id);
+        if (ids.length > 0) {
+          lastSeenId = Math.max(...ids);
+        }
+        this.scheduleMessages(msgContainer, resp.annotations);
+      })
       .receive("error", reason => console.log("join failed", reason));
   },
 
