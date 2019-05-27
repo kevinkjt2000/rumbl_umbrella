d6c7b4dbc5b37ba6e2684d5e90cc8dcd41e7b750
Kevin Tindall <kevinkjt2000@gmail.com>
2019-05-27 14:06:29 -0500

    Create wolfram backend for info sys

--- apps/info_sys/lib/info_sys/application.ex
+++ apps/info_sys/lib/info_sys/application.ex
@@ -8,12 +8,11 @@ defmodule InfoSys.Application do
   def start(_type, _args) do
     # List all child processes to be supervised
     children = [
-      {InfoSys.Counter, 5}
+      InfoSys.Supervisor
     ]
 
     # See https://hexdocs.pm/elixir/Supervisor.html
     # for other strategies and supported options
-    opts = [strategy: :one_for_one, name: InfoSys.Supervisor]
-    Supervisor.start_link(children, opts)
+    Supervisor.start_link(children, strategy: :one_for_one)
   end
 end
---
+++ apps/info_sys/lib/info_sys/supervisor.ex
@@ -0,0 +1,16 @@
+defmodule InfoSys.Supervisor do
+  use Supervisor
+
+  def start_link(opts) do
+    Supervisor.start_link(__MODULE__, opts, name: __MODULE__)
+  end
+
+  def init(_opts) do
+    children = [
+      InfoSys.Cache,
+      {Task.Supervisor, name: InfoSys.TaskSupervisor}
+    ]
+
+    Supervisor.init(children, strategy: :rest_for_one)
+  end
+end
