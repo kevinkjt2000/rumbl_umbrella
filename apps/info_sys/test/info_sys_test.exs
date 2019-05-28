b05698b74c6d16bf382e84da5b54fbfb51c6465d
Kevin Tindall <kevinkjt2000@gmail.com>
2019-05-27 21:56:59 -0500

    Tests for InfoSys.compute/2

--- apps/info_sys/test/info_sys_test.exs
+++ apps/info_sys/test/info_sys_test.exs
@@ -1,8 +1,47 @@
 defmodule InfoSysTest do
   use ExUnit.Case
-  doctest InfoSys
+  alias InfoSys.Result
 
-  test "greets the world" do
-    assert InfoSys.hello() == :world
+  defmodule TestBackend do
+    def start_link(query, ref, owner, limit) do
+      Task.start_link(__MODULE__, :fetch, [query, ref, owner, limit])
+    end
+
+    def name(), do: "Wolfram"
+
+    def compute("result", _opts) do
+      [%Result{backend: __MODULE__, text: "result"}]
+    end
+
+    def compute("none", _opts) do
+      []
+    end
+
+    def compute("timeout", _opts) do
+      :timer.sleep(:infinity)
+    end
+
+    def compute("boom", _opts) do
+      raise "boom!"
+    end
+  end
+
+  test "compute/2 with backend results" do
+    assert [%Result{backend: TestBackend, text: "result"}] =
+             InfoSys.compute("result", backends: [TestBackend])
+  end
+
+  test "compute/2 with no backend results" do
+    assert [] = InfoSys.compute("none", backends: [TestBackend])
+  end
+
+  test "compute/2 with timeout returns no results" do
+    results = InfoSys.compute("timeout", backends: [TestBackend], timeout: 10)
+    assert results == []
+  end
+
+  @tag :capture_log
+  test "compute/2 discards backend errors" do
+    assert InfoSys.compute("boom", backends: [TestBackend]) == []
   end
 end
