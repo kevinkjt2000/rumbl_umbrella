0d46a71484d05ff57cb4cd2b0f81cd2c920eee80
Kevin Tindall <kevinkjt2000@gmail.com>
2019-05-27 21:48:02 -0500

    Test more cache stuff

--- apps/info_sys/test/cache_test.exs
+++ apps/info_sys/test/cache_test.exs
@@ -19,4 +19,35 @@ defmodule InfoSysTest.CacheTest do
   test "unfound entry returns error", %{name: name} do
     assert Cache.fetch(name, :notexists) == :error
   end
+
+  test "clears all entries after clear interval", %{name: name} do
+    assert :ok = Cache.put(name, :key1, :value1)
+    assert Cache.fetch(name, :key1) == {:ok, :value1}
+    assert eventually(fn -> Cache.fetch(name, :key1) == :error end)
+  end
+
+  @tag clear_interval: 60_000
+  test "values are cleaned up on exit", %{name: name, pid: pid} do
+    assert :ok = Cache.put(name, :key1, :value1)
+    assert_shutdown(pid)
+    {:ok, _cache} = Cache.start_link(name: name)
+    assert Cache.fetch(name, :key1) == :error
+  end
+
+  defp assert_shutdown(pid) do
+    ref = Process.monitor(pid)
+    Process.unlink(pid)
+    Process.exit(pid, :kill)
+
+    assert_receive {:DOWN, ^ref, :process, ^pid, :killed}
+  end
+
+  defp eventually(func) do
+    if func.() do
+      true
+    else
+      Process.sleep(10)
+      eventually(func)
+    end
+  end
 end
